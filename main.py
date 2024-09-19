577ef5040e039fe078eea3f06737364282fe804c
AI-bot-1232 <nries2026@kehillahstudent.org>
2024-09-19 11:26:51 -0700

    finshed

--- main.py
+++ main.py
@@ -1,3 +1,141 @@
-import turtle
 
-turtle.forward(100)
+from turtle import *
+# number cube 1 
+# speed(10000000000)
+tracer(0,0)
+Y=5 
+pencolor("yellow")
+
+
+def draw_block():
+    for _ in range(4):
+        forward(100)
+        right(90)
+    penup()
+    right(90)
+    forward(50)
+    left(90)
+    forward(50)
+    pendown()
+    left(90)
+    forward(100)
+    for _ in range(4):
+        right(90)
+        forward(100)
+
+    right(180)
+    forward(100)
+    right(45)
+    forward(72)
+    right(135)
+    forward(101)
+    right(45)
+    forward(70)
+    right(45)
+    forward(100)
+    right(90) 
+    forward(100)
+    right(45)
+    forward(70)
+    right(45)
+    forward(100)
+    right(180)
+    forward(100)
+    left(90)
+    forward(100)
+    right(45)
+    forward(70)
+    # move back to start position
+    backward(70)
+    right(45)
+    backward(100)
+
+draw_block()
+# number cube 2
+# move to where next block starts
+forward(100)
+draw_block()
+
+# number cube 3 
+backward(200)
+draw_block()
+
+# number cube 4
+# move to where into the middle of the block
+pencolor("red")
+right(90)
+left(90)
+left(90)
+backward(100)
+forward(100)
+right(90)
+forward(100)
+left(90)
+left(90)
+forward(50)
+right(90)
+penup()
+forward(50)
+right(90)
+forward(50)
+left(90)
+right(90)
+# move to wehre into the middle of the block 
+penup()
+forward(100)
+left(90)
+left(90)
+forward(-Y)
+draw_block()
+right(90)
+forward(100)
+right(90)
+left(45+180)
+forward(73)
+left(45)
+left(90)
+draw_block()
+# penup()
+# forward(2)
+# penup()
+# right(90)
+# forward(100)
+# left(90)
+# forward(50)
+# right(270)
+# right(90)
+# penup()
+# right(900+90)
+# right(180)
+# forward(100)
+# right(270)
+# left(90)
+# right(180)
+# forward(100)
+# right(90)
+# forward(50)
+# right(90)
+# forward(100)
+# forward(50)
+# left(90)
+# forward(10)
+# right(90)
+# right(90)
+# left(90)
+# forward(100)
+# cube number 6
+pencolor("black")
+left(90)
+penup()
+forward(100)
+left(90)
+forward(50)
+right(180)
+draw_block()
+
+
+
+
+
+update()
+exitonclick()
